0fb2aa5a7030841c38d8a7df9f801698e0f91e2a
Paul <paul@goodhops.com>
2024-07-16 13:12:27 -0400

    Update category service to use Result pattern for return values using Ardalis.Result library.

--- src/FredDevelopmentKit/Services/FredHttpClient.cs
+++ src/FredDevelopmentKit/Services/FredHttpClient.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Json;
-using System.Text;
+﻿using System.Net.Http.Json;
 using System.Text.Json;
-using System.Threading.Tasks;
+using Ardalis.Result;
 
 namespace FredDevelopmentKit.Services
 {
@@ -17,14 +12,31 @@ namespace FredDevelopmentKit.Services
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri("https://api.stlouisfed.org/fred/");
         }
-        public async Task<T?> GetFromJsonAsync<T>(string url)
+        public async Task<Result<T>> GetFromJsonAsync<T>(string url)
         {
+            Result<T> result;
             var options = new JsonSerializerOptions
             {
                 // tell serializer response properties are in snake_case so they can be mapped to C# properties
                 PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
             };
-            return await _httpClient.GetFromJsonAsync<T?>(url, options);
+            try
+            {
+                T? response = await _httpClient.GetFromJsonAsync<T?>(url, options);
+                if (response == null)
+                {
+                    result = Result<T>.NotFound();
+                }
+                else
+                {
+                    result = Result<T>.Success(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                result = Result<T>.Error(ex.Message);
+            }
+            return result;
         }
     }
 }

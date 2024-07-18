6e14495977eabb387d4d78f26362ed34f16d8875
Paul <paul@goodhops.com>
2024-07-18 08:49:18 -0400

    Refactor examples, update README to reflect progress.

---
+++ src/FredDKSample/Examples/ReleasesExample.cs
@@ -0,0 +1,121 @@
+﻿using Ardalis.Result;
+using FredDevelopmentKit.Models;
+using FredDevelopmentKit.Services;
+
+namespace FredDKSample.Examples
+{
+    public static class ReleasesExample
+    {
+        public static async Task GetSampleReleasesData(IFredReleaseService? releaseService)
+        {
+            if (releaseService == null)
+            {
+                return;
+            }
+            ShowReleasesResults(await releaseService.GetReleases());
+            ShowReleasesDatesResults(await releaseService.GetReleasesDates());
+            ShowReleaseResult(await releaseService.GetRelease(19));
+            ShowReleaseDateResult(await releaseService.GetReleaseDates(19));
+            ShowRelatedSeriesResult(await releaseService.GetSeries(19));
+            ShowReleasesDatesResults(await releaseService.GetReleasesDates());
+            ShowSourcesResult(await releaseService.GetSources(19));
+            ShowTagsResult(await releaseService.GetTags(19));
+            ShowTagsResult(await releaseService.GetRelatedTags(19, new List<string> { "services", "quarterly" }));
+        }
+        public static void ShowReleasesResults(Result<ReleaseResponseDto> releasesResult)
+        {
+            if (releasesResult.IsSuccess)
+            {
+                foreach (Release release in releasesResult.Value.Releases)
+                {
+                    Console.WriteLine($"Release name is {release.Name} id is {release.Id}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Releases not found");
+            }
+        }
+        public static void ShowReleasesDatesResults(Result<ReleasesDatesResponseDto> releasesDatesResult)
+        {
+            if (releasesDatesResult.IsSuccess)
+            {
+                foreach (ReleasesDate releaseDate in releasesDatesResult.Value.ReleaseDates)
+                {
+                    Console.WriteLine($"Release date is {releaseDate.Date} id is {releaseDate.ReleaseId}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Releases dates not found");
+            }
+        }
+        public static void ShowReleaseResult(Result<Release> releaseResult)
+        {
+            if (releaseResult.IsSuccess)
+            {
+                Console.WriteLine($"Release name is {releaseResult.Value.Name} id is {releaseResult.Value.Id}");
+            }
+            else
+            {
+                Console.WriteLine("Release not found");
+            }
+        }
+        public static void ShowReleaseDateResult(Result<ReleaseDateResponseDto> releaseDatesResult)
+        {
+            if (releaseDatesResult.IsSuccess)
+            {
+                foreach (ReleaseDate releaseDate in releaseDatesResult.Value.ReleaseDates)
+                {
+                    Console.WriteLine($"Release date is {releaseDate.Date} id is {releaseDate.ReleaseId}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Release dates not found");
+            }
+        }
+        public static void ShowRelatedSeriesResult(Result<RelatedSeriesResponseDto> seriesResult)
+        {
+            if (seriesResult.IsSuccess)
+            {
+                foreach (SeriesDto series in seriesResult.Value.Series)
+                {
+                    Console.WriteLine($"Series name is {series.Title} id is {series.Id}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No series found");
+            }
+        }
+        public static void ShowSourcesResult(Result<SourcesResponseDto> sourcesResult)
+        {
+            if (sourcesResult.IsSuccess)
+            {
+                foreach (SourceDto source in sourcesResult.Value.Sources)
+                {
+                    Console.WriteLine($"Source name is {source.Name} id is {source.Id}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No sources found");
+            }
+        }
+        public static void ShowTagsResult(Result<TagsResponseDto> tagsResult)
+        {
+            if (tagsResult.IsSuccess)
+            {
+                foreach (TagDto tag in tagsResult.Value.Tags)
+                {
+                    Console.WriteLine($"Tag name is {tag.Name} Group id is {tag.GroupId}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No tags found");
+            }
+        }
+    }
+}
---
+++ src/FredDKSample/Examples/SeriesExample.cs
@@ -0,0 +1,108 @@
+﻿using Ardalis.Result;
+using FredDevelopmentKit.Models;
+using FredDevelopmentKit.Services;
+
+namespace FredDKSample.Examples
+{
+    public static class SeriesExample
+    {
+        public static async Task GetSampleSeriesData(IFredSeriesService? seriesService)
+        {
+            if (seriesService == null)
+            {
+                return;
+            }
+            ShowSeriesResult(await seriesService.GetSeries("GNPCA"));
+            ShowCategoriesResult(await seriesService.GetCategories("GNPCA"));
+            ShowObservationsResult(await seriesService.GetObservations("GNPCA"));
+            ReleasesExample.ShowReleaseResult(await seriesService.GetRelease("GNPCA"));
+            ShowSeriesResult(await seriesService.SearchSeries("monetary+service+index"));
+            ReleasesExample.ShowTagsResult(await seriesService.SearchSeriesTags("monetary+service+index"));
+            ReleasesExample.ShowTagsResult(await seriesService.SearchSeriesRelatedTags("monetary+service+index"));
+            ReleasesExample.ShowTagsResult(await seriesService.GetTags("GNPCA"));
+            ShowUpdateResult(await seriesService.GetSeriesUpdates());
+            ShowVintageDatesResult(await seriesService.GetVintageDates("GNPCA"));
+        }
+        public static void ShowSeriesResult(Result<SeriesDto> seriesResult)
+        {
+            if (seriesResult.IsSuccess)
+            {
+                Console.WriteLine($"Series name is {seriesResult.Value.Title} id is {seriesResult.Value.Id}");
+            }
+            else
+            {
+                Console.WriteLine("Series not found");
+            }
+        }
+        public static void ShowCategoriesResult(Result<CategoryResponseDto> categoriesResult)
+        {
+            if (categoriesResult.IsSuccess)
+            {
+                foreach (Category category in categoriesResult.Value.Categories)
+                {
+                    Console.WriteLine($"Category name is {category.Name} id is {category.Id}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No categories found");
+            }
+        }
+        public static void ShowObservationsResult(Result<ObservationsResponseDto> observationsResult)
+        {
+            if (observationsResult.IsSuccess)
+            {
+                foreach (ObservationDto observation in observationsResult.Value.Observations)
+                {
+                    Console.WriteLine($"Observation date is {observation.Date} value is {observation.Value}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No observations found");
+            }
+        }
+        public static void ShowSeriesResult(Result<SeriesResponseDto> seriesResult)
+        {
+            if (seriesResult.IsSuccess)
+            {
+                foreach (SeriesDto series in seriesResult.Value.Series)
+                {
+                    Console.WriteLine($"Series name is {series.Title} id is {series.Id}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No series found");
+            }
+        }
+        public static void ShowUpdateResult(Result<UpdateResponseDto> updatesResult)
+        {
+            if (updatesResult.IsSuccess)
+            {
+                foreach (SeriesDto series in updatesResult.Value.Series)
+                {
+                    Console.WriteLine($"Update name is {series.Title} id is {series.Id}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No updates found");
+            }
+        }
+        public static void ShowVintageDatesResult(Result<VintageDatesResponseDto> vintageDatesResult)
+        {
+            if (vintageDatesResult.IsSuccess)
+            {
+                foreach (string vintageDate in vintageDatesResult.Value.VintageDates)
+                {
+                    Console.WriteLine($"Vintage date is {vintageDate}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No vintage dates found");
+            }
+        }
+    }
+}
